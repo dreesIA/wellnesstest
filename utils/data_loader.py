7677824b6d0188c858f789a02f5ef6160a141283
dreesIA <161554052+dreesIA@users.noreply.github.com>
2025-08-22 06:34:35 -0400

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -20,7 +20,7 @@ def get_credentials():
     """
     # Try to load from Streamlit secrets first (for deployment)
     try:
-        if 'gcp_service_account' in st.secrets:
+        if 'service_account' in st.secrets:
             return ServiceAccountCredentials.from_json_keyfile_dict(
                 st.secrets["gcp_service_account"],
                 ['https://spreadsheets.google.com/feeds',
